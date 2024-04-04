6d8049df5b52b58bffa696bee6f4144afe5aaa6b
Raisa Silea <silearaisa@gmail.com>
2024-04-04 01:06:50 +0100

    initial Date class definition

--- Code/Utilities/Utilities.cpp
+++ Code/Utilities/Utilities.cpp
@@ -1,9 +1,38 @@
 #include "Utilities.h"
 
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <string>
+#include <iomanip>
+
 template <typename T>
 T getInput(){
     T input;
     std::cout << " > ";
     std::cin >> input;
     return input;
+}
+
+//initializes the member variable date with the 
+//value of the date parameter passed to the constructor.
+Date::Date(const std::string& date) : date(date) {}
+Date::Date() {}
+
+std::string Date::getDateOfBorrowal() const{
+    return date;
+}
+
+std::string Date::getDueDate(int rentalDays) const{
+    return date + "Due in " + rentalDays + "days";
+}
+
+void Date::setDateOfBorrowal(const std::string& aDate){
+    date = aDate;
+}
+
+void Date::setDueDate(const std::string& aDate){
+    date = aDate;
+    //will need to calculate amount of days in seconds
+    //then convert to days 
 }
--- Code/Utilities/Utilities.h
+++ Code/Utilities/Utilities.h
@@ -1,7 +1,28 @@
 #ifndef _UTILITIES_H_
 #define _UTILITIES_H_
+
 #include <iostream>
 #include <string>
 #include <vector>
+
+class Date{
+
+private:
+    std::string date; //date in string format 
+
+public:
+    Date(const std::string& date);
+    Date();
+
+    std::string getDateOfBorrowal() const;
+    std::string getDueDate() const;
+
+    void setDateOfBorrowal(const Date& aDate);
+    void setDueDate(const Date& aDate); 
+
+    
+}
+
+
 
 #endif
