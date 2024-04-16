394bfb4f9721ca47b820d2530a982f3c353ce678
KhalilM01 <162696476+KhalilM01@users.noreply.github.com>
2024-04-16 17:45:35 +0100

    Update DataReader.h - Added a test structure to store all the products from the csv and to see if they are saved appropriately and are displayed properly

--- Code/Data/DataReader.h
+++ Code/Data/DataReader.h
@@ -1,12 +1,25 @@
-#ifndef _DATAREADER_H_
-#define _DATAREADER_H_
+#ifndef DATA_READER_H
+#define DATA_READER_H
 #include "../Utilities/Utilities.h"
+#include <vector>
+#include <string>
 
-class DataReader{
-private:
-public:
-    DataReader();
-    ~DataReader();
+// Define a structure to hold product information
+struct Product {
+    int productID;
+    char gender;
+    std::string collection, productType, size, colour;
+    double dailyRentalPrice, totalPrice;
 };
 
-#endif
+// Declaration of the DataReader class
+class DataReader {
+public:
+    explicit DataReader(const std::string& filePath);
+    std::vector<Product> readProducts();
+
+private:
+    std::string filePath;
+};
+
+#endif // DATA_READER_H
