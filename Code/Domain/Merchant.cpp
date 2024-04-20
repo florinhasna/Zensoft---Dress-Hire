ac2e120a502ce1e1c2a575d33740bf0bb08e6abf
KhalilM01 <162696476+KhalilM01@users.noreply.github.com>
2024-04-20 16:08:16 +0100

    Updated Merchant.cpp - Added methods to get and set the PIN accordingly. StaffID is passed as a substitute to a regular customers ID since they are different.

--- Code/Domain/Merchant.cpp
+++ Code/Domain/Merchant.cpp
@@ -1,21 +1,35 @@
 #include "Merchant.h"
 
-Merchant::Merchant(const std::string& name, const std::string& email, const std::string& address, int staffID, int PIN)
-    : Person(name, email, address, staffID), PIN(PIN) {}
+Merchant::Merchant(const std::string& name, const std::string& email, const std::string& address, const std::string& staffID, const std::string& PIN)
+    : Person(name, email, address, staffID), // Now its passing staffID as ID to Person
+      PIN(PIN), 
+      staffID(staffID) {}
 
-int Merchant::getPIN() const {
+
+std::string Merchant::getPIN() const {
     return PIN;
 }
 
-void Merchant::setPIN(int newPIN) {
+void Merchant::setPIN(const std::string& newPIN) {
     PIN = newPIN;
 }
 
+std::string Merchant::getStaffID() const {
+    return staffID;
+}
+
+void Merchant::setStaffID(const std::string& newStaffID) {
+    staffID = newStaffID;
+}
+
 void Merchant::issueProduct(Customer& customer, Product& product) {
+    // Implementation for issuing a product to a customer
 }
 
 void Merchant::returnProduct(Customer& customer, Product& product) {
+    // Implementation for returning a product from a customer
 }
 
 void Merchant::calculateFine(Product& product) {
+    // Implementation for calculating fines
 }
