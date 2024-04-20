a160e425fcfe590bdf9de61bbb7ba1b86b70f273
KhalilM01 <162696476+KhalilM01@users.noreply.github.com>
2024-04-20 16:06:40 +0100

    Updated Merchant.h - Edited the constructor, made the parameters for StaffId and PIN into strings - Also made some getters and setters to fetch them when needed

--- Code/Domain/Merchant.h
+++ Code/Domain/Merchant.h
@@ -1,23 +1,28 @@
 #ifndef MERCHANT_H
 #define MERCHANT_H
 
-#include "Person.h" 
-#include "Customer.h" 
+#include "Person.h"
 #include "Product.h"
+#include "Customer.h" 
 
 class Merchant : public Person {
 private:
-    int PIN;
+    std::string PIN;  // PIN is a string so it can handle alphanumeric PINs
+    std::string staffID;  // Unique staff ID for the merchant which differs to the Customers ID
 
 public:
-    Merchant(const std::string& name, const std::string& email, const std::string& address, int staffID, int PIN);
+    // Constructor updated with PIN as std::string
+    Merchant(const std::string& name, const std::string& email, const std::string& address, const std::string& staffID, const std::string& PIN);
 
-    int getPIN() const;
-    void setPIN(int newPIN);
+    std::string getPIN() const;
+    void setPIN(const std::string& newPIN);
+
+    std::string getStaffID() const;  // Getter for staffID
+    void setStaffID(const std::string& newStaffID);  // Setter for staffID
 
     void issueProduct(Customer& customer, Product& product);
     void returnProduct(Customer& customer, Product& product);
     void calculateFine(Product& product);
 };
 
-#endif 
+#endif
