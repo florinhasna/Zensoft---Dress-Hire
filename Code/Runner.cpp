ebde4611ac8da650ab8c1343bb0e0c24038eafde
KhalilM01 <162696476+KhalilM01@users.noreply.github.com>
2024-04-26 14:07:32 +0100

    Update Runner.cpp - Borrowal and late test

--- Code/Runner.cpp
+++ Code/Runner.cpp
@@ -5,6 +5,14 @@
 #include "UserInterface/UserInterface.h"
 
 int main() {
+    //Test for Late and borrowal to show it works
+    //Product p(1, "Male", "Winter", "Jacket", "Warm Jacket", "L", "Black", 10.0, 100.0);
+    //p.setDateOfBorrowal("24-04-2024");
+
+//std::cout << "Total cost for borrowal: $" 
+          //<< std::fixed << std::setprecision(2) 
+          //<< p.calculateTotalForBorrowal() << std::endl;
+//std::cout << "Is the product late? " << (p.isLate() ? "Yes" : "No") << std::endl;
     DataReader dataReader("Products.csv", "Merchants.csv", "Customers.csv");
     UserInterface ui;
     Application application(ui, dataReader);
